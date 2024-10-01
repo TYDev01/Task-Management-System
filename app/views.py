899c0a21456bb90c0db8a54e6deebdda6826bc6b
TYDev <anthonychinaemeru84@yahoo.com>
2024-10-01 17:44:00 -0400

    Database connected, Users can register

--- app/views.py
+++ app/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello Tony")
+    return render(request, "index.html")
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, "signup.html", {"form": form})
